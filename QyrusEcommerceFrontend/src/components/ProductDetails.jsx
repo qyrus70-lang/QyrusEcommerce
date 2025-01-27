6c8a4f193b7dc9c44eb246d967572b5cd22164e3
hemanthsharmaqyrus <104136036+hemanthsharmaqyrus@users.noreply.github.com>
2025-01-27 12:32:54 +0530

    Update ProductDetails.jsx

--- QyrusEcommerceFrontend/src/components/ProductDetails.jsx
+++ QyrusEcommerceFrontend/src/components/ProductDetails.jsx
@@ -243,7 +243,7 @@ const ProductDetails = () => {
               }}
               className="px-4 py-2 bg-green-600 text-white rounded hover:bg-green-700"
             >
-              Get It!
+              Buy Buy Buy!
             </button>
 
             <button
