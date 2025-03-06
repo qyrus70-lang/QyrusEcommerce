2c4f26305a0768a858346ef8e9b4c0d45f2dfe5e
hemanthsqyrus <hemanths@qyrus.com>
2025-03-06 22:07:03 +0530

    Merge pull request #4 from hemanthsqyrus/feature/compare_products
    Feature/compare products

--- QyrusEcommerceFrontend/src/App.jsx
+++ QyrusEcommerceFrontend/src/App.jsx
@@ -14,6 +14,8 @@ import MyAddress from "./components/MyAddress";
 import AddressSelection from "./components/BuyNow";
 import PaymentPage from "./components/PaymentsPage";
 import MyOrders from "./components/MyOrders";
+import ComparePage from "./components/ComparePage";
+import TermsAndConditions from "./components/TermsAndConditions";
 
 
 const App = () => {
@@ -24,6 +26,7 @@ const App = () => {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/terms" element={<TermsAndConditions />} />
         <Route path="/products" element={<Products />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/" element={<Navigate to="/products" replace />} />
@@ -33,7 +36,7 @@ const App = () => {
         <Route path="/account" element={<AccountPage />} />
         <Route path="/contact" element={<ContactUs />} />
         <Route path="/my-address" element={<MyAddress />} />
-
+        <Route path="/compare" element={<ComparePage />} />
         <Route path="/buy-now" element={<AddressSelection />} />
         <Route path="/payment" element={<PaymentPage />} />
         <Route path="/my-orders" element={<MyOrders />} />
---
+++ QyrusEcommerceFrontend/src/components/TermsAndConditions.jsx
@@ -0,0 +1,22 @@
+const TermsAndConditions = () => {
+    return (
+      <div className="max-w-2xl mx-auto p-4">
+        <h1 className="text-3xl font-bold mb-4">Terms and Conditions</h1>
+        <p className="mb-4">
+          By using this application, you agree to the following terms and conditions.
+        </p>
+        <p className="mb-4">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices
+          eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate
+          semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.
+        </p>
+        <p className="mb-4">
+          Additional terms and conditions text goes here. Make sure to include all the details that
+          govern your application's use.
+        </p>
+      </div>
+    );
+  };
+  
+  export default TermsAndConditions;
+  
